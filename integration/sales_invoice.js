b910f6a7ddcc5224ac22d17f3638d10c6732f61e
Umair Sayed <umair@erpnext.com>
2019-10-16 11:43:25 +0530

    Update sales_invoice.js

--- integration/sales_invoice.js
+++ integration/sales_invoice.js
@@ -1,22 +1,29 @@
-context('Sales Invoice', () => {
+// Test case for creating a simple Sales Invoice, with partial payment entry.
+
+context('Table MultiSelect', () => {
 	beforeEach(() => {
 		cy.login();
 	});
 
 	it('Generate Sales Invoice', () => {
 		cy.new_form("Sales Invoice");
-		cy.fill_field("customer", "test", "Link");
+		cy.fill_field("Customer", "test", "Link");
+  		cy.fill_table("items", [{"item_code": "Test Item"} ,{"quantity": "2"}, {"basic_rate": "300"}]);
+		cy.save();
+		cy.fill_field("Sales Taxes and Charges Template", "In State", "Link");
+	    	cy.save();
+    		cy.submit();
+    		// Creating a payment entry against a Sales Invoice.
+    		cy.get_toolbar("Create", "Payment Entry");
 		cy.wait(1000);
-		cy.fill_field("account_id", "crescent.erpnext.com");
-		cy.fill_table("items", [{"item_code": "P25"}, {"item_code": "Paid Feature Development"}]);
-		cy.fill_field("remarks", "test remark");
+   		cy.fill_field("Mode of Payment", "Credit Card");
+    		// Ensure Paid To account is fetched after selecting mode of payment.
+    		cy.wait(100);
+    		cy.fill_field("Paid Amount", "500");
+    		cy.fill_field("reference_no", "UV6FU675");
+    		cy.fill_field("reference_date", "UV6FU675");
 		cy.save();
 		cy.submit();
-		cy.get_toolbar("Create", "Payment");
-		cy.fill_field("reference_no", "1234567890000");
-		cy.fill_field("reference_date", "10-10-2019", "Date");
-		cy.save();
-		cy.submit();
-		cy.get_button("Ledger");
+    		cy.get_button("Ledger");
 	});
 });
